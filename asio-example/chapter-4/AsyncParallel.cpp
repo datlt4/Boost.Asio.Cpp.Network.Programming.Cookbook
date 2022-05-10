e2043d151bc9db9c9dbdf99b66729381c9e28fbd
LuongTanDat <luongtandat.m0145@gmail.com>
2022-05-10 19:55:07 +0700

    update chapter 4

--- asio-example/chapter-4/AsyncParallel.cpp
+++ asio-example/chapter-4/AsyncParallel.cpp
@@ -1,239 +1,151 @@
-#include <boost/asio.hpp>
-
-#include <thread>
 #include <atomic>
-#include <memory>
+#include <boost/asio.hpp>
 #include <iostream>
-
-using namespace boost;
+#include <memory>
+#include <thread>
 
 class Service {
-public:
-	Service(std::shared_ptr<asio::ip::tcp::socket> sock) :
-		m_sock(sock)
-	{}
+   public:
+    Service(std::shared_ptr<boost::asio::ip::tcp::socket> sock) : m_sock(sock) {}
 
-	void StartHandling() {
+    void StartHandling() {
+        boost::asio::async_read_until(*m_sock.get(), m_request, '\n', [this](const boost::system::error_code& ec, std::size_t bytes_transferred) { onRequestReceived(ec, bytes_transferred); });
+    }
 
-		asio::async_read_until(*m_sock.get(),
-			m_request,
-			'\n',
-			[this](
-			const boost::system::error_code& ec,
-			std::size_t bytes_transferred)
-		{
-			onRequestReceived(ec,
-				bytes_transferred);
-		});
-	}
+   private:
+    void onRequestReceived(const boost::system::error_code& ec, std::size_t bytes_transferred) {
+        if (ec.value() != 0) {
+            std::cout << "Error occured! Error code = " << ec.value() << ". Message: " << ec.message();
+            onFinish();
+            return;
+        }
+        // Process the request.
+        m_response = ProcessRequest(m_request);
+        // Initiate asynchronous write operation.
+        boost::asio::async_write(*m_sock.get(), boost::asio::buffer(m_response), [this](const boost::system::error_code& ec, std::size_t bytes_transferred) { onResponseSent(ec, bytes_transferred); });
+    }
 
-private:
-	void onRequestReceived(const boost::system::error_code& ec,
-		std::size_t bytes_transferred) {
-		if (ec != 0) {
-			std::cout << "Error occured! Error code = "
-				<< ec.value()
-				<< ". Message: " << ec.message();
+    void onResponseSent(const boost::system::error_code& ec, std::size_t bytes_transferred) {
+        if (ec.value() != 0) {
+            std::cout << "Error occured! Error code = " << ec.value() << ". Message: " << ec.message();
+        }
+        onFinish();
+    }
 
-			onFinish();
-			return;
-		}
+    // Here we perform the cleanup.
+    void onFinish() { delete this; }
 
-		// Process the request.
-		m_response = ProcessRequest(m_request);
+    std::string ProcessRequest(boost::asio::streambuf& request) {
+        // In this method we parse the request, process it
+        // and prepare the request.
+        // Emulate CPU-consuming operations.
+        int i = 0;
+        while (i != 1000000) i++;
+        // Emulate operations that block the thread
+        // (e.g. synch I/O operations).
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        // Prepare and return the response message.
+        std::string response = "Response\n";
+        return response;
+    }
 
-		// Initiate asynchronous write operation.
-		asio::async_write(*m_sock.get(),
-			asio::buffer(m_response),
-			[this](
-			const boost::system::error_code& ec,
-			std::size_t bytes_transferred)
-		{
-			onResponseSent(ec,
-				bytes_transferred);
-		});
-	}
-
-	void onResponseSent(const boost::system::error_code& ec,
-		std::size_t bytes_transferred) {
-		if (ec != 0) {
-			std::cout << "Error occured! Error code = "
-				<< ec.value()
-				<< ". Message: " << ec.message();
-		}
-
-		onFinish();
-	}
-
-	// Here we perform the cleanup.
-	void onFinish() {
-		delete this;
-	}
-
-	std::string ProcessRequest(asio::streambuf& request) {
-
-		// In this method we parse the request, process it
-		// and prepare the request.
-
-		// Emulate CPU-consuming operations.
-		int i = 0;
-		while (i != 1000000)
-			i++;
-
-		// Emulate operations that block the thread
-		// (e.g. synch I/O operations).
-		std::this_thread::sleep_for(
-			std::chrono::milliseconds(100));
-
-		// Prepare and return the response message. 
-		std::string response = "Response\n";
-		return response;
-	}
-
-private:
-	std::shared_ptr<asio::ip::tcp::socket> m_sock;
-	std::string m_response;
-	asio::streambuf m_request;
+   private:
+    std::shared_ptr<boost::asio::ip::tcp::socket> m_sock;
+    std::string m_response;
+    boost::asio::streambuf m_request;
 };
 
 class Acceptor {
 public:
-	Acceptor(asio::io_service& ios, unsigned short port_num) :
-		m_ios(ios),
-		m_acceptor(m_ios,
-		asio::ip::tcp::endpoint(
-		asio::ip::address_v4::any(),
-		port_num)),
-		m_isStopped(false)
-	{}
+    Acceptor(boost::asio::io_service& ios, unsigned short port_num) : m_ios(ios), m_acceptor(m_ios, boost::asio::ip::tcp::endpoint(boost::asio::ip::address_v4::any(), port_num)), m_isStopped(false) {}
 
-	// Start accepting incoming connection requests.
-	void Start() {
-		m_acceptor.listen();
-		InitAccept();
-	}
+    // Start accepting incoming connection requests.
+    void Start() {
+        m_acceptor.listen();
+        InitAccept();
+    }
 
-	// Stop accepting incoming connection requests.
-	void Stop() {
-		m_isStopped.store(true);
-	}
+    // Stop accepting incoming connection requests.
+    void Stop() { m_isStopped.store(true); }
 
 private:
-	void InitAccept() {
-		std::shared_ptr<asio::ip::tcp::socket>
-			sock(new asio::ip::tcp::socket(m_ios));
+    void InitAccept() {
+        std::shared_ptr<boost::asio::ip::tcp::socket> sock(new boost::asio::ip::tcp::socket(m_ios));
+        m_acceptor.async_accept(*sock.get(), [this, sock](const boost::system::error_code& error) { onAccept(error, sock); });
+    }
 
-		m_acceptor.async_accept(*sock.get(),
-			[this, sock](
-			const boost::system::error_code& error)
-		{
-			onAccept(error, sock);
-		});
-	}
-
-	void onAccept(const boost::system::error_code& ec,
-		std::shared_ptr<asio::ip::tcp::socket> sock)
-	{
-		if (ec == 0) {
-			(new Service(sock))->StartHandling();
-		}
-		else {
-			std::cout << "Error occured! Error code = "
-				<< ec.value()
-				<< ". Message: " << ec.message();
-		}
-
-		// Init next async accept operation if
-		// acceptor has not been stopped yet.
-		if (!m_isStopped.load()) {
-			InitAccept();
-		}
-		else {
-			// Stop accepting incoming connections
-			// and free allocated resources.
-			m_acceptor.close();
-		}
-	}
+    void onAccept(const boost::system::error_code& ec, std::shared_ptr<boost::asio::ip::tcp::socket> sock) {
+        if (ec.value() == 0) {
+            (new Service(sock))->StartHandling();
+        } else {
+            std::cout << "Error occured! Error code = " << ec.value() << ". Message: " << ec.message();
+        }
+        // Init next async accept operation if
+        // acceptor has not been stopped yet.
+        if (!m_isStopped.load()) {
+            InitAccept();
+        } else {
+            // Stop accepting incoming connections
+            // and free allocated resources.
+            m_acceptor.close();
+        }
+    }
 
 private:
-	asio::io_service& m_ios;
-	asio::ip::tcp::acceptor m_acceptor;
-	std::atomic<bool> m_isStopped;
+    boost::asio::io_service& m_ios;
+    boost::asio::ip::tcp::acceptor m_acceptor;
+    std::atomic<bool> m_isStopped;
 };
 
 class Server {
 public:
-	Server() {
-		m_work.reset(new asio::io_service::work(m_ios));
-	}
+    Server() { m_work.reset(new boost::asio::io_service::work(m_ios)); }
 
-	// Start the server.
-	void Start(unsigned short port_num,
-		unsigned int thread_pool_size) {
+    // Start the server.
+    void Start(unsigned short port_num, unsigned int thread_pool_size) {
+        assert(thread_pool_size > 0);
+        // Create and start Acceptor.
+        acc.reset(new Acceptor(m_ios, port_num));
+        acc->Start();
+        // Create specified number of threads and
+        // add them to the pool.
+        for (unsigned int i = 0; i < thread_pool_size; i++) {
+            std::unique_ptr<std::thread> th(new std::thread([this]() { m_ios.run(); }));
+            m_thread_pool.push_back(std::move(th));
+        }
+    }
 
-		assert(thread_pool_size > 0);
-
-		// Create and start Acceptor.
-		acc.reset(new Acceptor(m_ios, port_num));
-		acc->Start();
-
-		// Create specified number of threads and 
-		// add them to the pool.
-		for (unsigned int i = 0; i < thread_pool_size; i++) {
-			std::unique_ptr<std::thread> th(
-                new std::thread([this]()
-                {
-                    m_ios.run();
-                }));
-
-			m_thread_pool.push_back(std::move(th));
-		}
-	}
-
-	// Stop the server.
-	void Stop() {
-		acc->Stop();
-		m_ios.stop();
-
-		for (auto& th : m_thread_pool) {
-			th->join();
-		}
-	}
+    // Stop the server.
+    void Stop() {
+        acc->Stop();
+        m_ios.stop();
+        for (auto& th : m_thread_pool) {
+            th->join();
+        }
+    }
 
 private:
-	asio::io_service m_ios;
-	std::unique_ptr<asio::io_service::work> m_work;
-	std::unique_ptr<Acceptor> acc;
-	std::vector<std::unique_ptr<std::thread>> m_thread_pool;
+    boost::asio::io_service m_ios;
+    std::unique_ptr<boost::asio::io_service::work> m_work;
+    std::unique_ptr<Acceptor> acc;
+    std::vector<std::unique_ptr<std::thread>> m_thread_pool;
 };
-
 
 const unsigned int DEFAULT_THREAD_POOL_SIZE = 2;
 
-int main()
-{
-	unsigned short port_num = 3333;
+int main() {
+    unsigned short port_num = 3333;
 
-	try {
-		Server srv;
+    try {
+        Server srv;
+        unsigned int thread_pool_size = std::thread::hardware_concurrency() * 2;
+        if (thread_pool_size == 0) thread_pool_size = DEFAULT_THREAD_POOL_SIZE;
+        srv.Start(port_num, thread_pool_size);
+        std::this_thread::sleep_for(std::chrono::seconds(60));
+        srv.Stop();
+    } catch (boost::system::system_error& e) {
+        std::cout << "Error occured! Error code = " << e.code() << ". Message: " << e.what();
+    }
 
-		unsigned int thread_pool_size =
-			std::thread::hardware_concurrency() * 2;
-
-		if (thread_pool_size == 0)
-			thread_pool_size = DEFAULT_THREAD_POOL_SIZE;
-
-		srv.Start(port_num, thread_pool_size);
-
-		std::this_thread::sleep_for(std::chrono::seconds(60));
-
-		srv.Stop();
-	}
-	catch (system::system_error &e) {
-		std::cout << "Error occured! Error code = "
-			<< e.code() << ". Message: "
-			<< e.what();
-	}
-
-	return 0;
+    return 0;
 }
--- asio-example/chapter-4/SyncIterative.cpp
+++ asio-example/chapter-4/SyncIterative.cpp
@@ -1,112 +1,83 @@
-#include <boost/asio.hpp>
-
-#include <thread>
 #include <atomic>
-#include <memory>
+#include <boost/asio.hpp>
 #include <iostream>
-
-using namespace boost;
+#include <memory>
+#include <thread>
 
 class Service {
 public:
-	Service(){}
+    Service() {}
 
-	void HandleClient(asio::ip::tcp::socket& sock) {
-		try {
-			asio::streambuf request;
-			asio::read_until(sock, request, '\n');
-
-			// Emulate request processing.
-			int i = 0;
-			while (i != 1000000)
-				i++;
-			std::this_thread::sleep_for(
-				std::chrono::milliseconds(500));
-
-			// Sending response.
-			std::string response = "Response\n";
-			asio::write(sock, asio::buffer(response));
-		}
-		catch (system::system_error &e) {
-			std::cout << "Error occured! Error code = "
-				<< e.code() << ". Message: "
-				<< e.what();
-		}
-	}
+    void HandleClient(boost::asio::ip::tcp::socket& sock) {
+        try {
+            boost::asio::streambuf request;
+            boost::asio::read_until(sock, request, '\n');
+            // Emulate request processing.
+            int i = 0;
+            while (i != 1000000) i++;
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+            // Sending response.
+            std::string response = "Response\n";
+            boost::asio::write(sock, boost::asio::buffer(response));
+        } catch (boost::system::system_error& e) {
+            std::cout << "Error occured! Error code = " << e.code() << ". Message: " << e.what();
+        }
+    }
 };
 
 class Acceptor {
 public:
-	Acceptor(asio::io_service& ios, unsigned short port_num) :
-		m_ios(ios),
-		m_acceptor(m_ios,
-		asio::ip::tcp::endpoint(
-		asio::ip::address_v4::any(),
-		port_num))
-	{
-		m_acceptor.listen();
-	}
+    Acceptor(boost::asio::io_service& ios, unsigned short port_num) : m_ios(ios), m_acceptor(m_ios, boost::asio::ip::tcp::endpoint(boost::asio::ip::address_v4::any(), port_num)) { m_acceptor.listen(); }
 
-	void Accept() {
-		asio::ip::tcp::socket sock(m_ios);
-
-		m_acceptor.accept(sock);
-
-		Service svc;
-		svc.HandleClient(sock);
-	}
+    void Accept() {
+        boost::asio::ip::tcp::socket sock(m_ios);
+        m_acceptor.accept(sock);
+        Service svc;
+        svc.HandleClient(sock);
+    }
 
 private:
-	asio::io_service& m_ios;
-	asio::ip::tcp::acceptor m_acceptor;
+    boost::asio::io_service& m_ios;
+    boost::asio::ip::tcp::acceptor m_acceptor;
 };
 
 class Server {
 public:
-	Server() : m_stop(false) {}
+    Server() : m_stop(false) {}
 
-	void Start(unsigned short port_num) {
-		m_thread.reset(new std::thread([this, port_num]() {
-			Run(port_num);
-		}));
-	}
+    void Start(unsigned short port_num) {
+        m_thread.reset(new std::thread([this, port_num]() { Run(port_num); }));
+    }
 
-	void Stop() {
-		m_stop.store(true);
-		m_thread->join();
-	}
+    void Stop() {
+        m_stop.store(true);
+        m_thread->join();
+    }
 
 private:
-	void Run(unsigned short port_num) {
-		Acceptor acc(m_ios, port_num);
+    void Run(unsigned short port_num) {
+        Acceptor acc(m_ios, port_num);
+        while (!m_stop.load()) {
+            acc.Accept();
+        }
+    }
 
-		while (!m_stop.load()) {
-			acc.Accept();
-		}
-	}
-
-	std::unique_ptr<std::thread> m_thread;
-	std::atomic<bool> m_stop;
-	asio::io_service m_ios;
+private:
+    std::unique_ptr<std::thread> m_thread;
+    std::atomic<bool> m_stop;
+    boost::asio::io_service m_ios;
 };
 
-int main()
-{
-	unsigned short port_num = 3333;
+int main() {
+    unsigned short port_num = 3333;
+    try {
+        Server srv;
+        srv.Start(port_num);
+        std::this_thread::sleep_for(std::chrono::seconds(60));
+        srv.Stop();
+    } catch (boost::system::system_error& e) {
+        std::cout << "Error occured! Error code = " << e.code() << ". Message: " << e.what();
+    }
 
-	try {
-		Server srv;
-		srv.Start(port_num);
-
-		std::this_thread::sleep_for(std::chrono::seconds(60));
-
-		srv.Stop();
-	}
-	catch (system::system_error &e) {
-		std::cout << "Error occured! Error code = "
-			<< e.code() << ". Message: "
-			<< e.what();
-	}
-
-	return 0;
+    return 0;
 }
